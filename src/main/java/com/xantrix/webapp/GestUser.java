4c86a433e99ce3a116897fbe76865b342ec219de
Antocar98 <antoniocarnevale924@gmail.com>
2024-09-19 09:24:24 +0200

    Initial commit

---
+++ src/main/java/com/xantrix/webapp/GestUser.java
@@ -0,0 +1,13 @@
+package com.xantrix.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestUser {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestUser.class, args);
+	}
+
+}
